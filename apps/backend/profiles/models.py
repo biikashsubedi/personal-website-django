92fbd9be278278fdfeff6253c303904ce4482802
Bikash Subedi <biikashsubedi@gmail.com>
2024-11-24 11:52:58 +0545

    new changes

--- apps/backend/profiles/models.py
+++ apps/backend/profiles/models.py
@@ -2,32 +2,33 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from ckeditor.fields import RichTextField
 
+
 # Create your models here.
 
 
 class Profile(models.Model):
     name = models.CharField(_("Name"), max_length=200, default='Bikash Subedi')
     introduction = RichTextField(blank=True, null=True)
-    job_title = models.CharField(_("Job Title"), max_length=200, default='Developer')
+    job_title = models.CharField(_("Job Title"), max_length=200, null=True, blank=True, default='Developer')
     image = models.ImageField(_('Image'), default='default/image.png', upload_to='profile')
     resume = models.FileField(_('Resume'), default='default/image.png', upload_to='profile/resume')
-    location = models.CharField(_("Location"), max_length=200, default='Kathmandu, Nepal')
-    phone1 = models.IntegerField(_("Phone 1"), default=9869286303)
-    phone2 = models.IntegerField(_("Phone 2"), default=9821241529)
-    completed_project = models.IntegerField(_("Completed Project"), default=1)
-    company_linked = models.IntegerField(_("Company Linked"), default=1)
+    location = models.CharField(_("Location"), max_length=200, null=True, blank=True, default='Kathmandu, Nepal')
+    phone1 = models.IntegerField(_("Phone 1"), null=True, blank=True, default=+9779869286303)
+    phone2 = models.IntegerField(_("Phone 2"), null=True, blank=True)
+    completed_project = models.IntegerField(_("Completed Project"), null=True, blank=True)
+    company_linked = models.IntegerField(_("Company Linked"), null=True, blank=True)
     love = models.IntegerField(_("Love"), default=1)
 
     email1 = models.CharField(_("Email 2"), max_length=200, default='biikashsubedi@gmail.com')
     email2 = models.CharField(_("Email 2"), max_length=200, default='biikashsubedi@outlook.com')
 
     website = models.CharField(_("Website"), max_length=200, default='bikashsubedi.com.np')
-    facebook_link = models.CharField(_("Facebook Link"), max_length=200, default='bikashsubedi.com.np')
-    linkedin_link = models.CharField(_("Linkedin Link"), max_length=200, default='bikashsubedi.com.np')
-    instagram_link = models.CharField(_("Instagram Link"), max_length=200, default='bikashsubedi.com.np')
-    twitter_link = models.CharField(_("Twitter Link"), max_length=200, default='bikashsubedi.com.np')
-    tor_link = models.CharField(_("Tor Link"), max_length=200, default='bikashsubedi.com.np')
-    gmap_link = models.CharField(_("Google Map Link"), max_length=500, default='bikashsubedi.com.np')
+    facebook_link = models.CharField(_("Facebook Link"), max_length=200, null=True, blank=True)
+    linkedin_link = models.CharField(_("Linkedin Link"), max_length=200, null=True, blank=True)
+    instagram_link = models.CharField(_("Instagram Link"), max_length=200, null=True, blank=True)
+    twitter_link = models.CharField(_("Twitter Link"), max_length=200, null=True, blank=True)
+    tor_link = models.CharField(_("Tor Link"), max_length=200, null=True, blank=True)
+    gmap_link = models.CharField(_("Google Map Link"), max_length=500, null=True, blank=True)
 
     status = models.BooleanField(_('status'), default=True)
     details = models.JSONField(_("details"), blank=True, null=True)
